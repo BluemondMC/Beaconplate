c91c810bf46e9a38a8a96d077784e4a3efa9c5d7
BluemondMC <128275690+BluemondMC@users.noreply.github.com>
2023-03-19 06:42:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ for (let i = 0; i < document.querySelectorAll("#RequestFullscreen").length; i++)
   const background = document.getElementById("background"),
   image = document.createElement('img'),
   ctx = background.getContext("2d");
-  image.src = 'assets/dirt.png';
+  image.src = 'dirt.png';
   let imgX = 0;
   
   image.onload = () => setInterval(() => {
