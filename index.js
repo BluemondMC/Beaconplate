54221ef74ccc72309aa280944945dbd5111430c4
BluemondMC <128275690+BluemondMC@users.noreply.github.com>
2023-06-16 15:29:50 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ var root = $("#root");
 //Screen navigator code
 const MainMenu = $("#MainMenu"),
 CreditsScreen = $("#CreditsScreen"),
-screens = [MainMenu,CreditsScreen];
+PackGeneratorScreen = $("#PackGeneratorScreen"),
+screens = [MainMenu,CreditsScreen,PackGeneratorScreen];
 
 function openScreen(which) {
     screens.forEach((v,i) => {
@@ -48,4 +49,51 @@ openScreen(MainMenu);
 function toggleFullscreen() {
     if(document.fullscreenElement) document.exitFullscreen();
     else document.documentElement.requestFullscreen();
+}
+
+function uuid() {
+    let a = "",
+    b = "0,1,2,3,4,5,6,7,8,9,a,b,c,d,e,f".split(",");
+
+    for (let i = 0; i < 8; i++) {
+        const c = b[Math.floor(Math.random() * b.length)];
+        a += c;
+    }
+    a+= "-";
+    for (let i = 0; i < 4; i++) {
+        const c = b[Math.floor(Math.random() * b.length)];
+        a += c;
+    }
+    a+= "-";
+    for (let i = 0; i < 4; i++) {
+        const c = b[Math.floor(Math.random() * b.length)];
+        a += c;
+    }
+    a+= "-";
+    for (let i = 0; i < 4; i++) {
+        const c = b[Math.floor(Math.random() * b.length)];
+        a += c;
+    }
+    a+= "-";
+    for (let i = 0; i < 12; i++) {
+        const c = b[Math.floor(Math.random() * b.length)];
+        a += c;
+    }
+
+    return a;
+}
+
+//Pack Generator code
+function GenerateManifestBedrock() {
+    const NameInput = PackGeneratorScreen.children[1],
+    DescInput = PackGeneratorScreen.children[2];
+
+    PackGeneratorScreen.children[4].innerText = JSON.stringify({
+        header: {
+            name: NameInput.value,
+            description: DescInput.value,
+            pack_version: [ 1, 0, 0 ],
+            uuid: uuid()
+        }
+    },2)
 }
