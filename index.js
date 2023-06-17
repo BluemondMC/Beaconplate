8b028320f7f86b144d86a7a8c62fd4a91d5a0ccb
BluemondMC <128275690+BluemondMC@users.noreply.github.com>
2023-06-17 12:18:27 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -35,7 +35,8 @@ var root = $("#root");
 const MainMenu = $("#MainMenu"),
 CreditsScreen = $("#CreditsScreen"),
 PackGeneratorScreen = $("#PackGeneratorScreen"),
-screens = [MainMenu,CreditsScreen,PackGeneratorScreen];
+ColorConverterScreen = $("#ColorConverterScreen"),
+screens = [MainMenu,CreditsScreen,PackGeneratorScreen,ColorConverterScreen];
 
 function openScreen(which) {
     screens.forEach((v,i) => {
@@ -86,14 +87,43 @@ function uuid() {
 //Pack Generator code
 function GenerateManifestBedrock() {
     const NameInput = PackGeneratorScreen.children[1],
-    DescInput = PackGeneratorScreen.children[2];
+    DescInput = PackGeneratorScreen.children[2],
+    TypeInput = PackGeneratorScreen.children[3];
 
-    PackGeneratorScreen.children[4].innerText = JSON.stringify({
+    PackGeneratorScreen.children[5].innerText = JSON.stringify({
+        format_version: 2,
         header: {
             name: NameInput.value,
             description: DescInput.value,
             pack_version: [ 1, 0, 0 ],
+            min_engine_version: [ 1, 20, 0 ],
             uuid: uuid()
-        }
+        },
+        modules: [
+            {
+                description: DescInput.value,
+                type: TypeInput.value,
+                uuid: uuid(),
+                min_engine_version: [ 0, 0, 1]
+            }
+        ]
     },2)
+}
+
+function convertColor() {
+    const Red = (parseFloat(ColorConverterScreen.children[1].value) !== null) ? parseFloat(ColorConverterScreen.children[1].value) : 255.0;
+    const Green = (parseFloat(ColorConverterScreen.children[2].value) !== null) ? parseFloat(ColorConverterScreen.children[2].value) : 255.0;
+    const Blue = (parseFloat(ColorConverterScreen.children[3].value) !== null) ? parseFloat(ColorConverterScreen.children[3].value) : 255;
+    const Alpha = (parseFloat(ColorConverterScreen.children[4].value) !== null) ? parseFloat(ColorConverterScreen.children[4].value) : 255;
+
+    ColorConverterScreen.children[6].innerText = JSON.stringify([ Red / 255, Green / 255, Blue / 255, Alpha / 255]);
+}
+
+function convertColorBackwards() {
+    const vec = JSON.parse(ColorConverterScreen.children[6].innerText);
+
+    ColorConverterScreen.children[1].value = Math.floor(vec[0])*255;
+    ColorConverterScreen.children[2].value = Math.floor(vec[1])*255;
+    ColorConverterScreen.children[3].value = Math.floor(vec[2])*255;
+    ColorConverterScreen.children[4].value = Math.floor(vec[3])*255;
 }
